72f650fa38b2633d1db4ef5d691a38799c3529d8
Praise <praisexi@outlook.com>
2023-12-01 18:58:06 +0000

    Add navigation menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+"use client"
+import  { NavigationBar } from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>Hello</div>
-  )
+    <div className="flex flex-col items-center justify-center w-full">
+      <a href="/" className="text-5xl font-bold mb-4">SHOTBYPRAISE</a>
+      <NavigationBar />
+
+    </div>
+  );
 }
