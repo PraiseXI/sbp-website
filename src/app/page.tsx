8fa1249b744094016a8024e51cc83617df76282c
Praise <praisexi@outlook.com>
2023-12-11 21:33:55 +0000

    Add new header component and update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 import  { NavigationBar } from "@/components/Navbar";
 import Slideshow from "../components/Slideshow"
+import Testimonials from "@/components/Testimonials";
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-full">
-      <a href="/" className="text-5xl font-bold mb-4">SHOTBYPRAISE</a>
-      <NavigationBar />
-      <Slideshow />
-    </div>
-  );
+
+    <main className={styles.main}>
+
+    </main>
+   );
 }
