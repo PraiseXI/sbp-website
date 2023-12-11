f23878cce60e2b90e55b08c7342a07dda831ba6a
Praise <praisexi@outlook.com>
2023-12-11 22:37:39 +0000

    Add body component to header nav

--- src/components/header/nav/index.tsx
+++ src/components/header/nav/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./style.module.scss";
 import { motion } from "framer-motion";
 import { height } from "../anim";
+import Body from "./body";
 
 export default function index() {
   return (
@@ -12,11 +13,7 @@ export default function index() {
       exit="exit"
     >
       <div className={styles.wrapper}>
-        <p>Nav</p>
-        <p>Nav</p>
-        <p>Nav</p>
-        <p>Nav</p>
-        <p>Nav</p>
+        <Body />
       </div>
     </motion.div>
   );
