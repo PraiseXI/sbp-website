e6abb08e3e2f95960e77a9a79325c3a3e8208ee9
Praise <praisexi@outlook.com>
2023-12-11 22:37:24 +0000

    Add Testimonials component

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,70 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/87o62ynAnyf
+ */
+export default function Testimonials() {
+    return (
+      <div className="py-12 bg-white dark:bg-gray-800">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+          <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl sm:leading-10 mb-10">
+            Testimonials
+          </h2>
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="group relative bg-white dark:bg-gray-700 shadow-xl rounded-lg p-8 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl border-2 border-transparent hover:border-gray-300 dark:hover:border-gray-600">
+              <img
+                alt="Person"
+                className="w-full h-64 object-cover mb-6 rounded-lg group-hover:scale-105 transition duration-300 ease-in-out transform"
+                height="256"
+                src="/placeholder.svg"
+                style={{
+                  aspectRatio: "256/256",
+                  objectFit: "cover",
+                }}
+                width="256"
+              />
+              <h3 className="text-xl font-bold text-gray-900 dark:text-white">Person Name</h3>
+              <p className="mt-2 text-base text-gray-500 dark:text-gray-400">
+                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, officia!"
+              </p>
+            </div>
+            <div className="group relative bg-white dark:bg-gray-700 shadow-xl rounded-lg p-8 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl border-2 border-transparent hover:border-gray-300 dark:hover:border-gray-600">
+              <img
+                alt="Person"
+                className="w-full h-64 object-cover mb-6 rounded-lg group-hover:scale-105 transition duration-300 ease-in-out transform"
+                height="256"
+                src="/placeholder.svg"
+                style={{
+                  aspectRatio: "256/256",
+                  objectFit: "cover",
+                }}
+                width="256"
+              />
+              <h3 className="text-xl font-bold text-gray-900 dark:text-white">Person Name</h3>
+              <p className="mt-2 text-base text-gray-500 dark:text-gray-400">
+                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, officia!"
+              </p>
+            </div>
+            <div className="group relative bg-white dark:bg-gray-700 shadow-xl rounded-lg p-8 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl border-2 border-transparent hover:border-gray-300 dark:hover:border-gray-600">
+              <img
+                alt="Person"
+                className="w-full h-64 object-cover mb-6 rounded-lg group-hover:scale-105 transition duration-300 ease-in-out transform"
+                height="256"
+                src="/placeholder.svg"
+                style={{
+                  aspectRatio: "256/256",
+                  objectFit: "cover",
+                }}
+                width="256"
+              />
+              <h3 className="text-xl font-bold text-gray-900 dark:text-white">Person Name</h3>
+              <p className="mt-2 text-base text-gray-500 dark:text-gray-400">
+                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, officia!"
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  
