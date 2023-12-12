4ff87f7612b77b1b2adbcea521288f67c68363f4
Praise <praisexi@outlook.com>
2023-12-12 23:45:24 +0000

    Remove unused Nav imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import  { NavigationBar } from "@/components/Navbar";
 import './globals.css'
 import Header from "../components/header/index"
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import  { NavigationBar } from "@/components/Navbar";
 import Slideshow from "../components/Slideshow"
 import Testimonials from "@/components/Testimonials";
 import styles from './page.module.css'
@@ -7,6 +6,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      HOMEPAGE
     </main>
    );
 }
