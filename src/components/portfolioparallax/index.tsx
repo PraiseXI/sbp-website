50d697a2af58c028760f2c1dae4e1af440772022
Praise <praisexi@outlook.com>
2023-12-21 20:18:21 +0000

    Update portfolio images

--- src/components/portfolioparallax/index.tsx
+++ src/components/portfolioparallax/index.tsx
@@ -12,18 +12,18 @@ interface ColumnProps {
 }
 
 const images: string[] = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
+  "2022.01.19 - Portrait - Aurelie-2.webp",
+  "2022.01.30 - Kaliya -33.webp",
+  "2022.01.30 - Kaliya -34.webp",
+  "2022.02.06 - Shaira-23.webp",
+  "Adeola - 15.04.21 - ShotbyPraise -42.webp",
+  "Kamisi - 25.02.21 - ShotbyPraise -19.webp",
+  "Sade - 16.08.21 - ShotbyPraise-3.webp",
+  "Sandra-Personal-4.webp",
+  "SBP-Alia-21.webp",
+  "SBP-Alia-38.webp",
+  "Tyez - 25.08.21 - ShotbyPraise-8.webp",
+  "2022.01.19 - Portrait - Aurelie-10.webp",
 ];
 
 export default function PortfolioParallax() {
