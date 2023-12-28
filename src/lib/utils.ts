ba0470d412f155946cadf234bd84235812894dc3
Praise <praisexi@outlook.com>
2023-12-28 21:11:19 +0000

    add export function cn and Fix formatting and add missing semicolons in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 import { supabase } from "@/supabase";
 import { imageItem } from "@/types";
- 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 const supabaseAdmin = supabase;
 
 export async function getBucketItems(bucketName: string): Promise<string[]> {
   try {
-    const { data, error } = await supabaseAdmin.storage
-    .from(bucketName)
-    .list();
+    const { data, error } = await supabaseAdmin.storage.from(bucketName).list();
     if (error) {
       console.error("Failed to fetch bucket items");
       return [];
@@ -23,7 +24,7 @@ export async function getBucketItems(bucketName: string): Promise<string[]> {
     );
     //randomise urls
     urls == urls.sort(() => Math.random() - 0.5);
-    return urls
+    return urls;
   } catch (error) {
     console.error("Error fetching bucket items:", error);
     return [];
@@ -45,4 +46,3 @@ export async function getImagePublicUrl(
     throw error;
   }
 }
-
